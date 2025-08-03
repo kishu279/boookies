21f64a2d609d37139943c4a44d7a5b56ebafd089
kishu279 <souravpoddar6677@gmail.com>
2025-08-03 18:15:09 +0530

    added onto the progress meter

---
+++ src/app/(home)/experimental/page.tsx
@@ -0,0 +1,10 @@
+import LogoBoookiess from "@/components/Logo";
+import Logo from "@/components/logo";
+
+export default function page() {
+  return (
+    <div className="flex w-full h-screen items-center justify-center">
+      <LogoBoookiess />
+    </div>
+  );
+}
